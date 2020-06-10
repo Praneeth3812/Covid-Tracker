0cba4048a9be0c1dc85cc48aa3094538897b4adc
PraneethNamala <praneeth.namala@credencys.com>
2020-06-10 10:34:12 +0530

    Updates

---
+++ precache-manifest.e3c572125ca1f65240f891fc11873893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e18442204794cad1127afa00ea4c27e",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "f108e43e9d761b7b3f6d",
+    "url": "/Covid-Tracker/static/css/main.cc1bec54.chunk.css"
+  },
+  {
+    "revision": "b4e5b92a7cf8f36a53d0",
+    "url": "/Covid-Tracker/static/js/2.368b4559.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker/static/js/2.368b4559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f108e43e9d761b7b3f6d",
+    "url": "/Covid-Tracker/static/js/main.2111ee0a.chunk.js"
+  },
+  {
+    "revision": "6ce6b1af7b7ec3b08be0",
+    "url": "/Covid-Tracker/static/js/runtime-main.9d2bd319.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
